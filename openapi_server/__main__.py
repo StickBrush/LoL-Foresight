cb7f0701a11ac5c0ad557c79c9395cc39315bbb6
StickBrush [zerø] <StickBrush@users.noreply.github.com>
2020-02-23 15:42:30 +0100

    Update __main__.py

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from openapi_server import encoder
 
 
 def main():
+    port = int(os.environ.get("PORT", 8080))
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'LoL prediction'},
                 pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=port)
 
 
 if __name__ == '__main__':
